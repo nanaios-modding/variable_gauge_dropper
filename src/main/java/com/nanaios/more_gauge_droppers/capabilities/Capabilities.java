149c2d4e4a5fd2553c0e377c3178f7d1428abf29
nanaios <game.ryd.131@gmail.com>
2025-09-10 22:29:22 +0900

    変更ミスを修正

--- src/main/java/com/nanaios/more_gauge_droppers/capabilities/Capabilities.java
+++
@@ -1,9 +0,0 @@
-package com.nanaios.more_gauge_droppers.capabilities;
-
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.common.capabilities.CapabilityToken;
-
-public class Capabilities {
-    public static final Capability<VariableGaugeDropperContentsHandler> VARIABLE_GAUGE_DROPPER_CONTENTS_HANDLER = CapabilityManager.get(new CapabilityToken<>() {});
-}
